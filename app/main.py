c13885f3b429dd5a00f80dc093b24c88d71d7161
CordeiroGusta <guscordeiro199@gmail.com>
2026-09-13 01:28:27 -0300

    feat: included API routes on main.py

--- app/main.py
+++ app/main.py
@@ -1,31 +1,8 @@
-# '''
-# QUAIS ENTIDADES QUE EU TENHO NA MINHA API:
-# itens:{
-#     'id' => identificador único do item cadastrado,
-#     'nome' => nome do item cadastrado,
-#     'descricao' => descricao breve sobre o item,
-#     'familia' => classificação a qual o item pertence,
-#     'valor_unitario' => valor unitario do item,
-#     'quantidade' => quantidade que tem desse item
-# }
-
-# QUAIS RECURSOS EU VOU DISPONIBILIZAR NESSA API:
-# todo o crud dos itens cadastrados
-#
-# ENDPOINTS;
-# - GET api/v1/items => busca geral por todos os itens
-# - GET api/v1/items/{id} => busca o item pelo seu id
-# - POST api/v1/items/ => Cria um novo item no armazenamento local
-# - PATCH api/v1/items/{id} => atualiza o item pelo seu id
-# - DELETE api/v1/items/{id} => deleta o item pelo seu id
-# '''
-
 from fastapi import FastAPI
+from app.routers import items_routes
 
 app = FastAPI(
-    title="teste"
+    title="Items CRUD API"
 )
 
-@app.get("/")
-def teste_api():
-    return "Ola, mundo!"
+app.include_router(items_routes.router)
