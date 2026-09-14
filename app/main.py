e76e4d83ef73d1eea55baf77a691149f73937cae
CordeiroGusta <guscordeiro199@gmail.com>
2026-09-13 22:44:18 -0300

    feat: add status endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from app.schemas.items_schemas import MessageResponse
 from app.routers import items_routes
 
 app = FastAPI(
-    title="Items CRUD API"
+    title="Items CRUD API",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
 )
 
 app.include_router(items_routes.router)
+
+@app.get(
+    '/api/v1',
+    status_code=status.HTTP_200_OK,
+    response_model=MessageResponse
+)
+def status_check():
+    return {'message': 'Olá, se você esta lendo isso, a API esta funcionando!'}
